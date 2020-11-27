bb59ec81bbe61b1d8c2349370faa41c0bcbb5ce2
msupb <eriksson.marcus15@gmail.com>
2020-11-27 10:58:26 +0100

    Test

--- CitationManager/Data/Commands/GetCitationsCommand.cs
+++ CitationManager/Data/Commands/GetCitationsCommand.cs
@@ -1,6 +1,11 @@
 ﻿using Data.Models;
 using Data.Repository;
+using Reference.Builder.Core;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Data.Commands
 {
